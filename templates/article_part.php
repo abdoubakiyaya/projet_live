094a6296034f6e0c38f455cdc8eda91e5a170a96
YAYA Abdou-baki <baki.abdou@yahoo.com>
2023-08-19 18:40:08 +0200

    version sans bdd

--- templates/article_part.php
+++ templates/article_part.php
@@ -1,10 +1,10 @@
 <div class="col">
   <div class="card h-100">
-    <img src="/uploads/articles/<?= $article["image"] ?>" class="card-img-top" alt="<?= $article["title"] ?>">
+    <img src="/uploads/articles/<?= htmlentities($article["image"]) ?>" class="card-img-top" alt="<?= htmlentities($article["title"]) ?>">
     <div class="card-body">
-      <h5 class="card-title"><?= $article["title"] ?></h5>
+      <h5 class="card-title"><?= htmlentities($article["title"]) ?></h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="actualite.php?id=<?= $key ?>" class="btn btn-primary">Lire la suite</a>
     </div>
   </div>
 </div>
