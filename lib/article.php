28ee2634667c5104eaea69fc4fc213a12f053bb0
YAYA Abdou-baki <baki.abdou@yahoo.com>
2023-08-21 14:53:28 +0200

    Requete artciles

--- lib/article.php
+++ lib/article.php
@@ -6,3 +6,22 @@ $articles = [
   ["title" => "Les meilleurs outils devops", "content" => " Outils Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolor vero ut, labore non deserunt quo blanditiis! Debitis id aliquid repellendus rem, provident eos quidem cum est eum, quam neque.", "image" => "3-devops.png"],
 
 ];
+
+function getArticles(PDO $pdo, int $limit = null): array
+{
+  $sql = "SELECT * FROM articles ORDER BY id DESC";
+  if ($limit) {
+    $sql .= " LIMIT :limit";
+  }
+
+  $query = $pdo->prepare($sql);
+
+  if ($limit) {
+    $query->bindValue(":limit", $limit, PDO::PARAM_INT);
+  }
+
+  $query->execute();
+  $articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  return $articles;
+}
--- templates/article_part.php
+++ templates/article_part.php
@@ -1,6 +1,17 @@
+<?php
+if ($article["image"] === null) {
+  $imagePath = _ASSETS_IMAGES_FOLDER_ . "default-article.jpg";
+} else {
+  $imagePath = _ARTICLES_IMAGES_FOLDER_ . htmlentities($article["image"]);
+}
+
+
+?>
+
+
 <div class="col">
   <div class="card h-100">
-    <img src="/uploads/articles/<?= htmlentities($article["image"]) ?>" class="card-img-top" alt="<?= htmlentities($article["title"]) ?>">
+    <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= htmlentities($article["title"]) ?>">
     <div class="card-body">
       <h5 class="card-title"><?= htmlentities($article["title"]) ?></h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
